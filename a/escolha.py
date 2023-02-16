cc929288c18f8ec0ba2c972b6caaa8809e67190b
Laís Fragoso <80287826+Laisfragoso@users.noreply.github.com>
2023-02-15 22:40:19 -0300

    Create escolha.py

---
+++ a/escolha.py
@@ -0,0 +1,8 @@
+opcao = input("Escolha uma opção (A/B): ")
+
+if opcao.upper() == "A":
+    print("Você escolheu a opção A. essa opção equivale a um date")
+elif opcao.upper() == "B":
+    print("Você escolheu a opção B. Equivale a um segundo date.")
+else:
+    print("Opção inválida. tem que sair comigo sim ")
